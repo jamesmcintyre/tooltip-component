54c95d5574b767e51eacd75b8178deee15143ccd
James McIntyre <james.checks.his.email@gmail.com>
2016-04-04 22:34:35 -0700

    scaffolding out files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+
+  var sampleData = {
+    name: 'Out of Galaxy',
+    logo: 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/321732-aee03e6d48b1bbe6bd83db6bb1296b1b-medium_jpg.jpg?buster=1389513653',
+    bio: 'Similar to a fitness tracker H2O-Pal is a hydration tracker that works with your water bottle. It makes your life easier by helping you drink more water and fewer calories. Many people mistreat the symptoms of dehydration using coffee and medicine, when what they really need is water.',
+    tags: 'Fitness'
+  };
+
+  
