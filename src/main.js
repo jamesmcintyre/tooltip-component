0dbce1aef42187c71ec9f697d4fe425b4c82dfa6
James McIntyre <james.checks.his.email@gmail.com>
2016-04-13 11:35:10 -0700

    integrate cyrils cust directive

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 var app = angular.module('toolTipApp', ['ui.bootstrap']);
 
 
-app.controller('mainController', function($scope, $http, $rootScope, $timeout) {
+app.controller('mainController', function($scope, $http, $rootScope) {
 
   console.log('loaded!');
 
@@ -16,45 +16,32 @@ app.controller('mainController', function($scope, $http, $rootScope, $timeout) {
         });
 
   $scope.hoverIn = function(activeRecord){
-    $scope.focusedElementIndex = this.$index;
-    this.activeRow = activeRecord;
+    $scope.activeRow = activeRecord;
   };
-
-  $scope.hideToolTip = function(index){
-      $scope.focusedElementIndex = null;
-  };
-
-  $scope.hideTest = function(inOutToggle){
-
-
-    if(inOutToggle === 'in'){
-      $scope.focusedElementState = true;
-    }else{
-      $scope.focusedElementState = false;
-      $timeout(function(){
-        if($scope.focusedElementState === false){
-          $scope.focusedElementIndex = null;
-        }
-      }, 500);
-    }
-
-  };
-
-  $scope.toggleTooltip = function(element){
-    if(this.$index === $scope.focusedElementIndex){
-      return true;
-    }else{
-      return false;
-    }
-  };
-
-
-  $scope.sampleData = {
-    name: 'Out of Galaxy',
-    logo: 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/321732-aee03e6d48b1bbe6bd83db6bb1296b1b-medium_jpg.jpg?buster=1389513653',
-    bio: 'Similar to a fitness tracker H2O-Pal is a hydration tracker that works with your water bottle. It makes your life easier by helping you drink more water and fewer calories. Many people mistreat the symptoms of dehydration using coffee and medicine, when what they really need is water.',
-    tags: ['Fitness', 'Great', 'Lovely']
-  };
-
 
 });
+
+app.directive('tooltip', function($compile, $templateRequest) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      // Get the template to put in the tooltip
+      $templateRequest(attrs.tooltip).then(function(html){
+
+          // Get the template
+          var template = angular.element(html);
+          $compile(template)(scope);
+          angular.element(document).append(template);
+
+          // Create the Drop (tooltip) element
+          drop = new Drop({
+            target: element[0],
+            content: template[0],
+            position: 'right top',
+            openOn: 'hover',
+            tetherOptions: {offset: '0 -10px'}
+          });
+      })
+    }
+  }
+})
